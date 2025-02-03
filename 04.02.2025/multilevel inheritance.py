4fbf62edb554c3ac6d0b32de7c15de7e931a364c
santhiyav23 <santhiyav2372006@gmail.com>
2025-02-03 16:21:16 +0530

    Create multilevel inheritance.py

---
+++ 04.02.2025/multilevel inheritance.py
@@ -0,0 +1,19 @@
+class grandpa:
+    def money(self):
+        print("grandpa money")
+class dad(grandpa):
+    def phone(self):
+        print("dad's phone")
+class mom:
+    def sweet(self):
+        print("mom's sweet")
+class son(dad,mom):
+    def laptop(self):
+        print("son's laptop")
+
+ram=son()
+ram.phone()
+ram.sweet() 
+
+d=dad()
+d.money()
