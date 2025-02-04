735b15549a9e492fdfb302130b21649b05517487
santhiyav23 <santhiyav2372006@gmail.com>
2025-02-04 13:33:21 +0530

    Create static method.py

---
+++ 04.02.2025/static method.py
@@ -0,0 +1,12 @@
+class MathUtility:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+
+
+print(MathUtility.add(3, 5))
+print(MathUtility.multiply(3, 5))
